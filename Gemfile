db6498e89a1d2d1c1376450ea303a3c5a66de32e
tranquangvu <vutran.513@gmail.com>
2018-06-07 09:26:18 +0700

    Add seeds

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
+gem 'bcrypt', '~> 3.1', '>= 3.1.12'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# create user
+ben = User.create(name: 'ben', email: 'ben@gmail.com', password: '12312312')
+
+# create links
+Link.create(url: 'http://graphql.org/', description: 'The Best Query Language', user: ben)
+Link.create(url: 'http://dev.apollodata.com/', description: 'Awesome GraphQL Client', user: ben)
