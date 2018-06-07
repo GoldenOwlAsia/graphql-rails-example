1aa19dc5cc83a699ee90c62b24b7f50e234ba15b
tranquangvu <vutran.513@gmail.com>
2018-06-07 09:16:53 +0700

    Init project and model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  has_many :votes
+  has_many :links
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+end
