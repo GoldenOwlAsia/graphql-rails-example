d76af479ee3eef56511e17655bee283439da0ecc
Phuoc <onhachungphuoc@gmail.com>
2018-08-04 09:45:44 +0700

    remove has secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_secure_password
 
   has_many :votes
   has_many :links
